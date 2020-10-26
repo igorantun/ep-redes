94b30ef9d4c1ac6a2fdf0b0bd9a6093905a903c3
Hezsel <vhesselp@gmail.com>
2020-10-26 12:47:59 -0300

    chore: document and clean up server

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ const fs = require('fs')
 const { v4: uuid } = require('uuid')
 const CronJob = require('cron').CronJob;
 const raffles = require('./raffles');
+
+// Apostas feitas nos sorteios
 const bets = {}
+
+// Clients conectados ao websocket
 const connections = new Map()
 
+// Carregando arquivos estaticos para serem servidos para o client
 let page, client, timers
 
 fs.readFile('src/index.html', (err, data) => {
@@ -21,12 +26,18 @@ fs.readFile('src/timers.js', (err, data) => {
   timers = data
 })
 
+// Funções auaxiliares
 const now = () => new Date().toISOString()
 
 const hasDuplicates = (numbers) => {
   return (new Set(numbers)).size !== numbers.length;
 }
 
+const getContentOfMessage = (message) => {
+  return JSON.parse(message.utf8Data)
+}
+
+// Envio de evento via websocket para o client
 const sendMessageToClient = (userId, type, content) => {
   const client = connections.get(userId)
     if (client) {
@@ -37,9 +48,9 @@ const sendMessageToClient = (userId, type, content) => {
     }
 }
 
-const sendBetAnnouncementToAllClients = (raffle, user) => {
+const sendBetAnnouncementToAllClients = (raffle, ignoredUser) => {
   connections.forEach((client, userId) => {
-    if (userId === user) return
+    if (userId === ignoredUser) return
 
     sendMessageToClient(userId, 'bet-made', { raffle })
   })
@@ -70,10 +81,11 @@ const isBetInvalid = (bet) => {
   return false
 }
 
-
+// Servidor http e rotas
 const server = http.createServer(async (req, res) => {
   console.log(`${now()} - ${req.method} request received at ${req.url}`)
 
+  // Rota de aposta
   if (req.method === 'POST' && req.url === '/bet') {
     await req.on('data', (chunk) => {
       const bet = JSON.parse(chunk)
@@ -95,6 +107,7 @@ const server = http.createServer(async (req, res) => {
 
   }
 
+  // Rotas para servir arquivos estáticos
   if (req.url === '/') {
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8',
@@ -126,15 +139,18 @@ const server = http.createServer(async (req, res) => {
   return res.end()
 })
 
+// Inicializando servidor http
+server.listen(3000, () => {
+  console.log(`🚀 Server ready at :3000`)
+})
+
+// Servidor WebSocket
 webSocketServer = new webSocket({
   httpServer: server,
 })
 
-const getContentOfMessage = (message) => {
-  return JSON.parse(message.utf8Data)
-}
-
 webSocketServer.on('request', (req) => {
+  // recebendo uma conexão nova no websocket
   const connection = req.accept(null, req.origin)
   const connectionId = uuid()
   connections.set(connectionId, connection)
@@ -142,19 +158,18 @@ webSocketServer.on('request', (req) => {
 
   console.log(`${now()} - ${connection.remoteAddress} connected`)
 
+  // Recebendo evento de message do client (apenas usado para testes)
   connection.on('message', (message) => {
     console.log(getContentOfMessage(message))
   })
 
+  // Recebendo evento de close do client
   connection.on('close', () => {
     console.log(`${now()} - ${connection.remoteAddress} disconnected`)
   })
 })
 
-server.listen(3000, () => {
-  console.log(`🚀 Server ready at :3000`)
-})
-
+// Funções referentes aos sorteios
 const drawNumbers = ({ range, draws }) => {
   const numbers = []
   while(numbers.length < draws){
@@ -175,6 +190,7 @@ const countBetHits = (draw, bet) => {
 }
 
 const givePrizes = (raffle, draw, raffleBets) => {
+  // Percorre todas as apostas e verifica quais acertaram o suficiente para ter ganhar algo
   for (const bet of raffleBets) {
     const hits = countBetHits(draw, bet.numbers)
     if (raffle.prize[hits] > 0) {
@@ -190,17 +206,20 @@ const givePrizes = (raffle, draw, raffleBets) => {
 }
 
 const createRaffle = (raffle) => new CronJob(raffle.interval, function() {
-  const draw = drawNumbers(raffle)
+  // Codigo do sorteio que será rodado após o tempo definido para apostas acabar
   if (!bets[raffle.name] || bets[raffle.name].length < 1) {
     console.log(`${now()} - No bets made in lottery "${raffle.name}"`)
     return
   }
+
+  const draw = drawNumbers(raffle)
 
   console.log(`${now()} - ${raffle.name}: (${bets[raffle.name].length} bets) - ${draw}`)
   givePrizes(raffle, draw, bets[raffle.name])
   bets[raffle.name] = []
 }, null, true, 'America/Sao_Paulo')
 
+// Inicializando Sorteios baseado nos dados de sorteios em src/raffles.js
 const startRaffles = () => {
   const onGoingRaffles = {}
   for (const [name, raffle] of Object.entries(raffles)) {
